f770483eaeff436eea17872318e87b9e7f2fbb45
MrZorg111 <yodji333@mail.ru>
2023-03-05 13:50:03 +0300

    New Solution

--- Task_2.cpp
+++ Task_2.cpp
@@ -1,44 +1,54 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <iomanip>
-#include <sstream>
-
-struct data_happy {
-    std::string name = " ";
-    double diff = 0;
-};
+#include <map>
+#include <vector>
 
 
 int main() {
-    std::vector<data_happy> d_h  ;
+    std::map<std::time_t, std::vector<std::string>> data_birthday;
+    std::vector<std::string> name_happy;
     std::string name;
-    std::time_t happy_birthday = std::time(nullptr);
-    std::tm* local = std::localtime(&happy_birthday);
-    std::time_t present_time = std::time(nullptr);
-
-    std::cout << "Enter your friend's name and date of birth:\n";
-    std::cout << "Name: ";
+    std::time_t tempo;
+    std::time_t happy= std::time(nullptr);
+    std::tm local = *localtime(&happy);
+    std::cout << "Enter your friend's name and date of birth:";
+    do {
+    std::cout << "\nName ";
     std::cin >> name;
-    for( int i = 0; name != "end"; i++) {
-        d_h.resize(i);
-        d_h.push_back(data_happy());
 
-        std::cout << "Date: ";
-        std::cin >> std::get_time(local, "%Y/%m/%d");
+    if (name == "end") break;
 
-        d_h[i].name = name;
-        d_h[i].diff = std::difftime(std::mktime(local), present_time);
-
-        std::cout << "Enter your friend's name and date of birth:\n";
-        std::cin >> name;
+    std::cout << "Date ";
+    std::cin >> std::get_time(&local, "%Y/%m/%d");
+    tempo = std::mktime(&local);
+    tempo = tempo - happy;
+    if(tempo < 0) {
+        continue;
     }
+    else {
+        if (data_birthday.count(tempo)) {
+            data_birthday.find(tempo)->second.push_back(name);
+        } else {
+            name_happy.clear();
+            name_happy.push_back(name);
+            data_birthday.insert(std::pair<std::time_t, std::vector<std::string>>(tempo, name_happy));
+        }
+    }
+    } while(true);
 
-    for (int l = 0; l < d_h.size(); l++) {
-        std::cout << d_h[l].name << " " << d_h[l].diff << "\n";
+    if (data_birthday.begin() -> first == 0) {
+        std::cout << "Don't forget to congratulate today: ";
+        for (int i = 0; i < data_birthday.begin() -> second.size(); i++) {
+            std::cout << data_birthday.begin() -> second[i] << " ";
+        }
 
     }
-
+    else {
+        std::cout << "Upcoming birthdays for: ";
+        for (int i = 0; i < data_birthday.begin() -> second.size(); i++) {
+            std::cout << data_birthday.begin() -> second[i] << " ";
+        }
+    }
     return 0;
 }
-///ESSSOLVENT
