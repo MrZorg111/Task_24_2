08b67b685a99c1c16b0b67b23b617bdac90c15ff
MrZorg111 <yodji333@mail.ru>
2023-03-09 14:29:05 +0300

    Final test close

--- Task_2.cpp
+++ Task_2.cpp
@@ -4,6 +4,18 @@
 #include <map>
 #include <vector>
 
+bool check_date (std::tm a, std::tm b) {
+    if (a.tm_year < b.tm_year) {
+        return false;
+    }
+    if (a.tm_year <= b.tm_year && a.tm_mon < b.tm_mon) {
+        return false;
+    }
+    if (a.tm_year <= b.tm_year && a.tm_mon <= b.tm_mon && a.tm_mday < b.tm_mday) {
+        return false;
+    }
+    return true;
+}
 
 int main() {
     std::map<std::time_t, std::vector<std::string>> data_birthday;
@@ -11,6 +23,7 @@ int main() {
     std::string name;
     std::time_t tempo;
     std::time_t happy= std::time(nullptr);
+    std::tm happy_tm = *localtime(&happy);
     std::tm local = *localtime(&happy);
     std::cout << "Enter your friend's name and date of birth:";
     do {
@@ -21,6 +34,10 @@ int main() {
 
     std::cout << "Date ";
     std::cin >> std::get_time(&local, "%Y/%m/%d");
+    if (!check_date(local, happy_tm)) {
+        std::cout << "Date is incorrect, please try again.\n";
+        continue;
+    }
     tempo = std::mktime(&local);
     tempo = (std::time_t)std::difftime(tempo, happy);
 
@@ -37,22 +54,22 @@ int main() {
         }
     }
     } while(true);
+    if (!data_birthday.empty()) {
+        if (data_birthday.begin()->first == 0) {
+            std::cout << "Don't forget to congratulate today: ";
+            for (int i = 0; i < data_birthday.begin()->second.size(); i++) {
+                std::cout << data_birthday.begin()->second[i] << " ";
+            }
 
-    if (data_birthday.begin() -> first == 0) {
-        std::cout << "Don't forget to congratulate today: ";
-        for (int i = 0; i < data_birthday.begin() -> second.size(); i++) {
-            std::cout << data_birthday.begin() -> second[i] << " ";
+        } else {
+            std::cout << "Upcoming birthdays for: ";
+            for (int i = 0; i < data_birthday.begin()->second.size(); i++) {
+                std::cout << data_birthday.begin()->second[i] << " ";
+            }
+            tempo = data_birthday.begin()->first + happy;
+            std::tm t = *localtime(&tempo);
+            std::cout << std::put_time(&t, "%Y/%m/%d");
         }
-
-    }
-    else {
-        std::cout << "Upcoming birthdays for: ";
-        for (int i = 0; i < data_birthday.begin() -> second.size(); i++) {
-            std::cout << data_birthday.begin() -> second[i] << " ";
-        }
-        tempo = data_birthday.begin() -> first + happy;
-        std::tm t = *localtime(&tempo);
-        std::cout << std::put_time(&t, "%Y/%m/%d");
     }
     return 0;
 }
